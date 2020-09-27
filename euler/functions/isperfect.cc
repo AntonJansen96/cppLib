605de7581e4a2f1e4c3de905c2b8e9bbd0f0e590
Anton Jansen <antonjansen96@gmail.com>
2020-09-27 15:59:24 +0200

    moved data out of function

--- euler/functions/isperfect.cc
+++ euler/functions/isperfect.cc
@@ -1,14 +1,19 @@
 #include "../euler.h"
 #include <algorithm>
 
-bool euler::isPerfect(size_t num)
-{   // Data so make static so we don't define it every time function is called.
-    static std::vector<size_t> const pNums = 
-    {   
-        6, 28, 496, 8128, 33550336, 8589869056, 137438691328, 
-        2305843008139952128
-    };
+namespace {
 
+// Data so make static so we don't define it every time function is called.
+static std::vector<size_t> const pNums = 
+{   
+    6, 28, 496, 8128, 33550336, 8589869056, 137438691328, 
+    2305843008139952128
+};
+
+}
+
+bool euler::isPerfect(size_t num)
+{   
     return
     (
         std::find(pNums.begin(), pNums.end(), num) != pNums.end()
