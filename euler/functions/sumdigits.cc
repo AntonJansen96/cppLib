6e46664858fb55be67f083ba11666d3f78d13ba9
Anton Jansen <antonjansen96@gmail.com>
2020-10-01 00:18:35 +0200

    removed unnecessary floor

--- euler/functions/sumdigits.cc
+++ euler/functions/sumdigits.cc
@@ -1,5 +1,4 @@
 #include "../euler.h"
-#include <cmath>
 
 // Returns the sum of the digits of num.
 int euler::sumDigits(size_t num)
@@ -9,7 +8,7 @@ int euler::sumDigits(size_t num)
     while (num)
     {
         count += num % 10;
-        num = std::floor(num / 10);
+        num /= 10;
     }
     return count;
 }
