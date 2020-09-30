19b7d0bca2cf680108d7ec78e4cab982164c6a21
Anton Jansen <55056286+AntonJansen96@users.noreply.github.com>
2020-10-01 00:26:42 +0200

    Update euler.h

--- euler/euler.h
+++ euler/euler.h
@@ -16,7 +16,7 @@ namespace euler {
 // Returns the number of digits of a number.
 int numDigits(size_t num);
 
-// Returns the sum of the digits of num.
+// Returns the sum of the digits of number.
 int sumDigits(size_t num);
 
 // Returns the first N digits from a number.
@@ -30,24 +30,24 @@ size_t lastNdigits(size_t num, int N);
 
 // CHECKS FOR CERTAIN PROPERTIES ***********************************************
 
-// Check whether num is a square.
+// Checks whether number is a perfect square.
 bool isSquare(size_t num);
 
-// Check whether two numbers a and b are coprime.
+// Checks whether two numbers a and b are coprime.
 bool isCoprime(size_t a, size_t b);
 
-// Check whether num is a perfect number.
+// Checkss whether number is a perfect number.
 bool isPerfect(size_t num);
 
-// Checks whether a number is juf.
+// Checks whether number is juf.
 bool isJuf(size_t num);
 
-// Check whether a number is a palindrome.
+// Checks whether number is a palindrome.
 bool isPalindrome(size_t num);
 
 // PARTITIONING ****************************************************************
 
-// Return #partitions of a number.
+// Returns #partitions of a number.
 double partition(size_t money);
 
 // Returns #ways money can be divided by coins in listed in coinvector.
@@ -55,17 +55,17 @@ double partition(size_t money, std::vector<size_t> const &coins);
 
 // GENERATORS ******************************************************************
 
-// Generate all Fibonacci numbers smaller than uplim.
+// Generates all Fibonacci numbers smaller than uplim.
 std::vector<size_t> genFibonnaci(size_t uplim);
 
-// Generate all numbers that are a to b pandigital.
+// Generates all numbers that are a to b pandigital.
 std::vector<size_t> genPandigital(int a, int b);
 
-// Generate primitive Pythagorean triples with perimeter (a + b + c) < perimLim.
+// Generates primitive Pythagorean triples with perimeter (a + b + c) < perimLim.
 std::tuple<std::vector<size_t>, std::vector<size_t>, std::vector<size_t>> 
 genPrimTriples(size_t perimLim);
 
-// Generate all Pythagorean triples with perimeter (a + b + c) < perimLim.
+// Generates all Pythagorean triples with perimeter (a + b + c) < perimLim.
 // Contains duplicate perimeters.
 std::tuple<std::vector<size_t>, std::vector<size_t>, std::vector<size_t>> 
 genTriples(size_t perimLim);
@@ -83,13 +83,13 @@ int roman2dec(std::string const &numeral);
 
 // MISCELLANEOUS FUNCTIONS *****************************************************
 
-// Euclidean algorithm. Returns greatest common denominator of a and b.
+// Euclidean algorithm. Returns the greatest common denominator of a and b.
 int gcd(int a, int b);
 
-// Returns factorial of number.
+// Returns the factorial of number.
 double factorial(int num);
 
-// Returns binomial coefficient of N and n.
+// Returns the binomial coefficient of N and n.
 double binomCoeff(int N, int n);
 
 } // Namespace.
