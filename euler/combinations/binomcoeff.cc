0cd6b5c9fbe7968f4a72131499c6205871464726
anton <anton@tcbl13.scilifelab.se>
2020-10-06 14:23:49 +0200

    merged binomcoeff.cc and factorial.cc

--- euler/combinations/binomcoeff.cc
+++ euler/combinations/binomcoeff.cc
@@ -1,5 +1,17 @@
 #include "combinations.h"
 
+namespace {
+
+double factorial(double num)
+{
+    if (num > 1)
+        return num * factorial(num - 1);
+    else
+        return 1;
+}
+
+} // Namespace.
+
 // Private function to compute binomial coefficient.
 double euler::Combinations::binomCoeff(size_t N, size_t n) const
 {
