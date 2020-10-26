b3ec534dd51f7bc9664620ddd4e04d4e4a86f50a
anton <anton@tcbl13.scilifelab.se>
2020-10-26 13:45:50 +0100

    changed comment

--- euler/functions/iscoprime.cc
+++ euler/functions/iscoprime.cc
@@ -3,7 +3,7 @@
 // Checks whether two numbers a and b are coprime.
 bool euler::isCoprime(int a, int b)
 {
-    if (((a | b) & 1) == 0) // Reject if both are even (gcd(a,b) >= 2.
+    if (((a | b) & 1) == 0) // Reject if both are even (gcd(a,b) >= 2).
         return false;
     
     return (gcd(a, b) == 1);
