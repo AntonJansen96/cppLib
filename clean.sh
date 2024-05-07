ab7e89a38bef1a15a383bc534dbc8a6874b24a14
Anton Jansen <antonjansen96@gmail.com>
2024-05-07 22:52:53 +0200

    update

--- clean.sh
+++ clean.sh
@@ -2,4 +2,6 @@
 
 find . -name 'CMakeFiles' | xargs rm -rf
 find . -name 'cmake_install.cmake' | xargs rm -f
+find . -name 'main' | xargs rm -f
 find . -name '*.a' | xargs rm -f
+find . -name '*.o' | xargs rm -f
