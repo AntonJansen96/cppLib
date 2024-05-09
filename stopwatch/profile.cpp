6bc95512fd77b86341b2b23cdc01aea9d18fbcdf
Anton Jansen <antonjansen96@gmail.com>
2024-05-09 18:45:53 +0200

    improve profile

--- stopwatch/profile.cpp
+++ stopwatch/profile.cpp
@@ -1,35 +1,27 @@
 #include "stopwatch.h"
+#include "../math/math.h"
+#include "../pythonlike/pythonlike.h"
+#include <vector>
 
-namespace {
-enum : size_t
-{   // Number of iterations for determining the internal time.
-    INTERNAL_ITER = 1'000'000 
-};
-}
+using namespace pythonlike;
 
 // Additional function that may be be used for profiling functions/lambdas.
 // Make sure to inline the function to be tested.
 size_t profile(size_t cycles, void (*func)())
 {
-    Stopwatch clock1; // For internal time.
-    Stopwatch clock2; // For execution time.
-    
-    for (size_t idx = 0; idx != INTERNAL_ITER; ++idx)
+    std::vector<size_t> values(cycles);
+
+    for (size_t idx = 0; idx != cycles; ++idx)
     {
-        clock1.start();
-        clock1.stop();
+        timePoint const start = std::chrono::system_clock::now();
+        func();
+        timePoint const end = std::chrono::system_clock::now();
+        values[idx] = (end - start).count();
     }
 
-    clock2.start();
-    for (size_t idx = 0; idx != cycles; ++idx)
-        func();
-    clock2.stop();
+    double const mean = math::mean(values);
+    double const sdev = math::sdev(values);
 
-    size_t const internaltime = clock1.rawtime() / INTERNAL_ITER;
-    size_t const exectime = (clock2.rawtime() - internaltime) / cycles;
-
-    std::cout << "profile: average execution time is " << exectime
-        << " (" << cycles << " cycles).\n";
-
-    return exectime;
+    print("profile: average execution time is", mean, "±", sdev, "ns (", cycles, "cycles ).");
+    return static_cast<size_t>(mean);
 }
