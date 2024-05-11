183cbb16c7263f1e6ca3b73b8d83c8808e6b57c7
Anton Jansen <antonjansen96@gmail.com>
2024-05-11 16:00:16 +0200

    do not use pythonlike in tests - we test lib separately

--- tests/test_euler.cpp
+++ tests/test_euler.cpp
@@ -1,7 +1,4 @@
 #include "euler/euler.h"
-#include "pythonlike/pythonlike.h"
-
-using namespace pythonlike;
 
 int main()
 {
--- tests/test_fileio.cpp
+++ tests/test_fileio.cpp
@@ -1,7 +1,4 @@
 #include "fileio/fileio.h"
-#include "pythonlike/pythonlike.h"
-
-using namespace pythonlike;
 
 int main()
 {
