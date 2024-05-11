2e1d1d16e86ed1821679314192182ca169cf2022
Anton Jansen <antonjansen96@gmail.com>
2024-05-11 17:35:57 +0200

    add proper tests for fractions

--- tests/test_fractions.cpp
+++ tests/test_fractions.cpp
@@ -1,64 +1,34 @@
 #include "fractions/fractions.h"
-#include <iostream>
+#include <cassert>
 
 int main()
-{
-    auto frac1 = Fraction(1, 1);
-    auto frac2 = Fraction(-2, 4);
-    std::cout << Fraction(1, 3) << '\n';
-    ;
-    std::cout << Fraction(1, 3).approx() << '\n';
-    ;
-    std::cout << Fraction(2, 4) << '\n';
-    ;
-    std::cout << Fraction(-1, 3) << '\n';
-    ;
-    std::cout << Fraction(1, -3) << '\n';
-    ;
-    std::cout << Fraction(-1, -3) << '\n';
-    ;
-    std::cout << Fraction(-1, 0) << '\n';
-    ;
+{    
+    // Test whether normalize() works properly.
+    assert(Fraction(2, 4) == Fraction(1, 2));
+    assert(Fraction(-2, -4) == Fraction(1, 2));
+    assert(Fraction(2, -4) == Fraction(-1, 2));
 
-    std::cout << frac1 + frac2 << '\n';
-    std::cout << frac1 - frac2 << '\n';
-    std::cout << frac1 * frac2 << '\n';
-    std::cout << frac1 / frac2 << '\n';
+    // Test approximation.
+    assert(Fraction(1, 3).approx() == 0.3333333333333333);
 
-    std::cout << 4 + frac2 << '\n';
-    ;
-    std::cout << -4 + frac2 << '\n';
-    ;
-    std::cout << 4 - frac2 << '\n';
-    ;
-    std::cout << -4 - frac2 << '\n';
-    ;
-    std::cout << 4 * frac2 << '\n';
-    ;
-    std::cout << -4 * frac2 << '\n';
-    ;
+    // Test addition, subtraction, multiplication, and division.
+    Fraction frac1(1, 2);
+    Fraction frac2(3, 4);
+    assert(frac1 + frac2 == Fraction(5, 4));
+    assert(frac1 - frac2 == Fraction(-1, 4));
+    assert(frac1 * frac2 == Fraction(3, 8));
+    assert(frac1 / frac2 == Fraction(2, 3));
+    assert(frac1 + 2 == Fraction(5, 2));
+    assert(frac1 - 2 == Fraction(-3, 2));
+    assert(5 * frac1 == Fraction(5, 2));
+    assert(4 / frac2 == Fraction(16, 3));
+    assert(frac2 / 4 == Fraction(3, 16));
 
-    std::cout << 4 / frac2 << '\n';
-    ; // should return -8
-    std::cout << -4 / frac2 << '\n';
-    ; // should return 8
-    std::cout << frac2 / 4 << '\n';
-    ; // should return -1/8
-    std::cout << frac2 / -4 << '\n';
-    ; // should return 1/8
-    std::cout << (frac2 /= -4) << '\n';
-    ; // should return 1/8
-
-    std::cout << (frac1 < frac2) << '\n';
-    ;
-    std::cout << (frac1 > frac2) << '\n';
-    ;
-    std::cout << (frac1 == frac2) << '\n';
-    ;
-    std::cout << (frac1 != frac2) << '\n';
-    ;
-    std::cout << (frac1 >= frac2) << '\n';
-    ;
-    std::cout << (frac1 <= frac2) << '\n';
-    ;
+    // Test comparison operators.
+    assert((Fraction(1, 2) < Fraction(3, 4)) == true);
+    assert((Fraction(1, 2) > Fraction(3, 4)) == false);
+    assert((Fraction(1, 2) == Fraction(3, 4)) == false);
+    assert((Fraction(1, 2) != Fraction(3, 4)) == true);
+    assert((Fraction(1, 2) >= Fraction(3, 4)) == false);
+    assert((Fraction(1, 2) <= Fraction(3, 4)) == true);
 }
