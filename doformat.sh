594c5ce0c2ea2ee298674cc9eb93644bce22829f
Anton Jansen <antonjansen96@gmail.com>
2024-05-14 01:34:39 +0200

    only format modified files

--- doformat.sh
+++ doformat.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-find . -iname \*.cpp -o -iname \*.h | xargs clang-format -i
+# Run for entire repository.
+# find . -iname \*.cpp -o -iname \*.h | xargs clang-format -i
+
+# Run for just the modified files.
+git diff --name-only '*.cpp' '*.h' | xargs clang-format -i
