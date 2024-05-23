acd505281eeb7fdc7a1157f3b76df408f7da5328
Anton Jansen <antonjansen96@gmail.com>
2024-05-23 12:35:28 +0200

    improve
    Now runs for modified and new .cpp and .h files, but not deleted ones.

--- doformat.sh
+++ doformat.sh
@@ -3,5 +3,8 @@
 # Run for entire repository.
 # find . -iname \*.cpp -o -iname \*.h | xargs clang-format -i
 
-# Run for just the modified files.
-git diff --name-only '*.cpp' '*.h' | xargs clang-format -i
+# Run clang-format for modified, but not deleted files.
+git diff --name-only --diff-filter=d '*.cpp' '*.h' | xargs clang-format -i
+
+# Run clang-format for untracked (new) files.
+git ls-files --others --exclude-standard '*.cpp' '*.h' | xargs clang-format -i
